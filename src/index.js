e3796c4971ee2caa739d52c981204922089e637a
Shamel Lakin <christlife4u@gmail.com>
2021-06-27 14:08:50 -0400

    Board render refactor

--- src/index.js
+++ src/index.js
@@ -30,19 +30,9 @@ class Board extends React.Component {
     );
   }
 
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner:' + winner;
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-     
-
+  render() { 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
